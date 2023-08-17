d13290ca962fe3612e5bc669d64de8de89995683
David <davidlebeau64@hotmail.com>
2023-08-17 15:32:34 +0200

    optimisations 4.1

--- src/components/home/GenerateRatingStars.js
+++ src/components/home/GenerateRatingStars.js
@@ -4,27 +4,27 @@ import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 // Fonction pour générer les étoiles en fonction du rating
 const GenerateRatingStars = (rating) => {
-  const filledStars = [];
-  const emptyStars = [];
+    const filledStars = [];
+    const emptyStars = [];
 
-  for (let i = 0; i < rating; i++) {
-    filledStars.push(
-      <FontAwesomeIcon key={i} icon={faStar} className='star-filled' />
+    for (let i = 0; i < rating; i++) {
+        filledStars.push(
+            <FontAwesomeIcon key={i} icon={faStar} className='star-filled' />
+        );
+    }
+
+    for (let i = rating; i < 5; i++) {
+        emptyStars.push(
+            <FontAwesomeIcon key={i} icon={faStar} className='star-empty' />
+        );
+    }
+
+    return (
+        <div className='rating'>
+            {filledStars}
+            {emptyStars}
+        </div>
     );
-  }
-
-  for (let i = rating; i < 5; i++) {
-    emptyStars.push(
-      <FontAwesomeIcon key={i} icon={faStar} className='star-empty' />
-    );
-  }
-
-  return (
-    <div className='rating'>
-      {filledStars}
-      {emptyStars}
-    </div>
-  );
 };
 
 export default GenerateRatingStars;
